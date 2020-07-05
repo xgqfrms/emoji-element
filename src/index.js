55f34d22fbb7b1c90375305d3ac6581d85bae412
xgqfrms <xgqfrms@outlook.com>
2020-07-06 00:52:47 +0800

    npm test

---
+++ src/index.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/**
+ * 
+ * @author xgqfrms
+ * @license MIT
+ * @copyright xgqfrms
+ * @created 2020-07-01
+ * @modified 
+ * 
+ * @description emoji-element
+ * @description Web Components & Custom Elements 
+ * @augments 
+ * @example 
+ * @link 
+ * 
+ */
+
+const log = console.log;
+
+log(`hello, emoji-element!`)
