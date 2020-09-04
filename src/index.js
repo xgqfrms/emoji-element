a6795527f48cba3b35d09b2f67749dc20bba83b4
xgqfrms <xgqfrms@outlook.com>
2020-09-05 00:10:16 +0800

    ES-Next: static property / public class field 🚀

--- src/index.js
+++ src/index.js
@@ -19,61 +19,58 @@
 const log = console.log;
 
 log(`hello, emoji-element!`);
-
-// html template 或 html template string
-const template = document.createElement('template');
-// :host for Shadow DOM style
-template.innerHTML = `
-  <style>
-    :host {
-      display: block;
-      position: relative;
-    }
-    #image,
-    #placeholder ::slotted(*) {
-      position: absolute;
-      top: 0;
-      left: 0;
-      transition:
-        opacity
-        var(--emoji-element-fade-duration, 0.3s)
-        var(--emoji-element-fade-easing, ease);
-      object-fit: var(--emoji-element-fit, contain);
-      width: var(--emoji-element-width, 100%);
-      height: var(--emoji-element-height, 100%);
-    }
-    :host([fade]) #placeholder:not([aria-hidden="true"]) ::slotted(*),
-    :host([fade]) #image:not([aria-hidden="true"]) {
-      opacity: 1;
-    }
-    :host([fade]) #image,
-    :host([fade]) #placeholder[aria-hidden="true"] ::slotted(*) {
-      opacity: 0;
-    }
-  </style>
-  <div id="placeholder" aria-hidden="false">
-    <slot name="placeholder"></slot>
-  </div>
-  <img id="image" aria-hidden="true"/>
-  <button>click</button>
-`;
-
-const tagName = `emoji-element`;
-
 class EmojiElement extends HTMLElement {
   constructor() {
     super();
     this.onLoad = this.onLoad.bind(this);
     this.onError = this.onError.bind(this);
+    log(`this.shadowRoot`, this.shadowRoot);
+    log(`this.shadow`, this.shadow);
     // show Shadow DOM
     this.shadowEmoji = this.attachShadow({mode: 'open'});
     // hidden Shadow DOM
     // this.attachShadow({mode: 'close'});
-    const templateContent = template.content.cloneNode(true);
-    log(`templateContent`, templateContent)
-    this.template = templateContent;
-    log(`this.template`, this.template)
-    this.shadowEmoji.appendChild(templateContent);
+    log(`this.shadowRoot === this.shadowEmoji`, this.shadowRoot === this.shadowEmoji);
+    // html template 或 html template string
+    const template = document.createElement('template');
+    // :host for Shadow DOM style
+    template.innerHTML = `
+      <style>
+        :host {
+          display: block;
+          position: relative;
+        }
+        #image,
+        #placeholder ::slotted(*) {
+          position: absolute;
+          top: 0;
+          left: 0;
+          transition:
+            opacity
+            var(--emoji-element-fade-duration, 0.3s)
+            var(--emoji-element-fade-easing, ease);
+          object-fit: var(--emoji-element-fit, contain);
+          width: var(--emoji-element-width, 100%);
+          height: var(--emoji-element-height, 100%);
+        }
+        :host([fade]) #placeholder:not([aria-hidden="true"]) ::slotted(*),
+        :host([fade]) #image:not([aria-hidden="true"]) {
+          opacity: 1;
+        }
+        :host([fade]) #image,
+        :host([fade]) #placeholder[aria-hidden="true"] ::slotted(*) {
+          opacity: 0;
+        }
+      </style>
+      <div id="placeholder" aria-hidden="false">
+        <slot name="placeholder"></slot>
+      </div>
+      <img id="image" aria-hidden="true"/>
+      <button>click</button>
+    `;
+    this.templateContent = template.content.cloneNode(true);
+    log(`this.templateContent`, this.templateContent)
+    this.shadowEmoji.appendChild(this.templateContent);
     this.shadowPlaceholder = this.shadowEmoji.getElementById('placeholder');
     // this.appendChild(templateContent);
     // this.shadowRoot.appendChild(templateContent);
@@ -84,7 +81,14 @@ class EmojiElement extends HTMLElement {
       // do something
       log(`clicked button`)
     });
+    // 添加样式到 Shadow DOM （template）
+    // if (window.ShadyCSS) {
+    //   window.ShadyCSS.prepareTemplate(template, EmojiElement.tagName);
+    // }
   }
+
+  // ES-Next: static property / public class field 🚀
+  static tagName = `emoji-element`;
 
   safeSetAttribute(name, value) {
     if (this.getAttribute(name) !== value) {
@@ -158,7 +162,16 @@ class EmojiElement extends HTMLElement {
 }
 
 
-// 添加样式到 Shadow DOM （template）
-if (window.ShadyCSS) window.ShadyCSS.prepareTemplate(template, tagName);
 
-customElements.define(tagName, EmojiElement);
+if(!EmojiElement.tagName) {
+  // ES6: static property 🚀
+  EmojiElement.tagName = `emoji-element`;
+}
+
+
+// 添加样式到 Shadow DOM （template）
+// if (window.ShadyCSS) {
+//   window.ShadyCSS.prepareTemplate(template, EmojiElement.tagName);
+// }
+
+customElements.define(EmojiElement.tagName, EmojiElement);
